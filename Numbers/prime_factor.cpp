7d72dc508ed31f97b7692fe3ac5e7705086eb302
Muhammad Sofyan <octa7th@gmail.com>
2014-11-06 10:26:28 +0700

    Add solution for prime_factor

---
+++ Numbers/prime_factor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int smallestFactor(int number) {
+    for (int i = 2; i <= number; ++i) {
+        if (number % i == 0) return i;
+    }
+    return number;
+}
+
+int main() {
+    int num, factor;
+    std::cout << "Please enter a number: ";
+    std::cin >> num;
+    std::cout << "You select " << num << "\n";
+    factor = smallestFactor(num);
+
+    std::cout << "Factors = ";
+
+    while (factor != num) {
+        std::cout << factor << " x ";
+        num = num / factor;
+        factor = smallestFactor(num);
+    }
+
+    std::cout << factor << "\n";
+
+    return 1;
+}
